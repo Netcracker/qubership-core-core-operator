fec292ab37afa249f036fd19af8086f895ab12b8
Evgenii Stetsenko <i@estetsenko.com>
2025-12-12 11:16:10 +0300

    feat: fix subtree cleanup

--- service/src/main/java/com/netcracker/core/declarative/service/NoopCompositeConsulUpdaterImpl.java
+++ service/src/main/java/com/netcracker/core/declarative/service/NoopCompositeConsulUpdaterImpl.java
@@ -3,6 +3,7 @@ package com.netcracker.core.declarative.service;
 import com.netcracker.core.declarative.exception.NoopConsulException;
 
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
 
 public class NoopCompositeConsulUpdaterImpl implements CompositeConsulUpdater {
 
@@ -12,12 +13,13 @@ public class NoopCompositeConsulUpdaterImpl implements CompositeConsulUpdater {
     }
 
     @Override
-    public Set<String> getCompositeMembers(String compositeId) {
+    public void updateCompositeStructureInConsul(String namespace, CompositeSpec compositeSpec) throws ExecutionException, InterruptedException {
         throw new NoopConsulException();
     }
 
     @Override
-    public void setNamespace(String namespace) {
+    public Set<String> getCompositeMembers(String compositeId) {
         throw new NoopConsulException();
     }
+
 }
---
+++ service/src/test/java/com/netcracker/core/declarative/service/CompositeReconcilerConsulTest.java
@@ -0,0 +1,34 @@
+package com.netcracker.core.declarative.service;
+
+import com.netcracker.core.declarative.ConsulTestResource;
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@QuarkusTest
+@QuarkusTestResource(value = ConsulTestResource.class)
+public class CompositeReconcilerConsulTest {
+
+    @Inject
+    CompositeConsulUpdater compositeConsulUpdater;
+
+    @Test
+    public void deleteSubTree() throws Exception {
+        compositeConsulUpdater.updateCompositeStructureInConsul("test-namespace-baseline", new CompositeSpec("", "test-namespace-baseline", "", null));
+        Set<String> compositeMembers = compositeConsulUpdater.getCompositeMembers("test-namespace-baseline");
+        assertTrue(compositeMembers.contains("test-namespace-baseline"));
+
+        compositeConsulUpdater.updateCompositeStructureInConsul("test-namespace", new CompositeSpec("", "test-namespace", "", new CompositeSpec.CompositeSpecBaseline("", "test-namespace-baseline", "")));
+        compositeMembers = compositeConsulUpdater.getCompositeMembers("test-namespace-baseline");
+        assertTrue(compositeMembers.contains("test-namespace-baseline"));
+
+        compositeConsulUpdater.updateCompositeStructureInConsul("test-namespace", new CompositeSpec("", "test-namespace", "", new CompositeSpec.CompositeSpecBaseline("", "test-namespace-baseline", "")));
+        compositeMembers = compositeConsulUpdater.getCompositeMembers("test-namespace-baseline");
+        assertTrue(compositeMembers.contains("test-namespace-baseline"));
+    }
+}
